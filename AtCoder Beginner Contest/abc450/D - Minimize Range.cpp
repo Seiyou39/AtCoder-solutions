2767a9929c3017939705fba81fbff2e82c041bf4
Seiyou39 <39jingyang.zhang@gmail.com>
2026-03-26 00:01:23 +0900

    Add comment in abc450 D

--- AtCoder Beginner Contest/abc450/D - Minimize Range.cpp
+++ AtCoder Beginner Contest/abc450/D - Minimize Range.cpp
@@ -3,6 +3,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+//ring buffer solution, time complexity O(N log N) due to sorting, space complexity O(N)
+//there is a ring and the size is K because  every number is reduced modulo K
+//like K = 10 and we have 1, 3, 15, 29, then we have 1, 3, 5, 9 in the ring
+//we need to find the maximum gap between two adjacent numbers in the ring
+//so that we could put the numbers in the same order in the ring and minimize the range, which is K - max_gap
+//like 1, 3, 5, 9, the maximum gap is 4 between 5 and 9
+//the size of ring is 10, which means[1*, 2, 3*, 4, 5*, 6, 7, 8, 9*, 0]
+//cut the ring between 5 and 9, let the array become more crowded, which is [9*, 0, 1*, 2, 3*, 4, 5*, 6, 7, 8]
+//the answer is 10 - 4 = 6
+
+
+
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
